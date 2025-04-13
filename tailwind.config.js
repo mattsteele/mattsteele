4d5b7cb7be8e564d99511fe49749665ed1176f86
Matt Steele <mattsteele000@gmail.com>
2025-04-12 20:32:55 -0600

    Remove tailwind config

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-// 	darkMode: 'class', // Enable class-based dark mode
-// 	content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
-// 	theme: {
-// 		extend: {},
-// 	},
-// 	plugins: [],
-// };
