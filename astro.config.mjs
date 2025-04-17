5340d9c771ebc7ff571679fad6911beaa834ebe8
Matt Steele <mattsteele000@gmail.com>
2025-04-16 21:43:04 -0600

    Add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import partytown from '@astrojs/partytown';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://www.mattsteele.dev'
+
+  site: 'https://www.mattsteele.dev',
+  integrations: [partytown({ config: { forward: ['dataLayer.push'] } })],
 });
