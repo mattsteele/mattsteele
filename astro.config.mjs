da51319820ef192c8357889342e353a0c093702a
Matt Steele <mattsteele000@gmail.com>
2025-11-27 07:13:48 -0700

    Update styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import partytown from '@astrojs/partytown';
 
 export default defineConfig({
